208d1e3b72a94174c8795c2941836364f2b1ebfc
Mike <80673026+mssj-11@users.noreply.github.com>
2023-04-13 16:59:27 -0600

    Spring Boot - MongoDB - API CRUD

---
+++ src/main/java/com/mss/springboot/web/app/SpringBootMongodbApiCrudApplication.java
@@ -0,0 +1,13 @@
+package com.mss.springboot.web.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMongodbApiCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMongodbApiCrudApplication.class, args);
+	}
+
+}
